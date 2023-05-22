c1a8dee09b13f1718a1520bab170f06ddad05eea
muskan12388 <134252709+muskan12388@users.noreply.github.com>
2023-05-22 05:09:30 +0000

    changesAssignment1.cs

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,49 +1,36 @@
 ﻿namespace Assignment
 {
+
     public static class ArrayReplicator
     {
-        /// <summary>
-        /// Replicates (deep copies) the incoming array
-        /// </summary>
-        /// <param name="original">The array to be replicated</param>
-        /// <returns>A deep copy of the original array</returns>
         public static int[] ReplicateArray(int[] original)
         {
-            int size = original.Length;
-            int[] copyArray = new int[size];
-            // LINQ -> map
-            for (int i = 0; i < size; ++i)
+            int content = original.Length;
+            int[] copyArray = new int[content];
+          
+
+            for (int i = 0; i < content; ++i)
             {
                 copyArray[i] = original[i];
             }
             return copyArray;
         }
 
-        /// <summary>
-        /// Asks the user for a number. Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <returns>The user input as an integer</returns>
+        
         public static int AskForNumber(string text)
         {
             Console.Write(text);
-            string userInput = Console.ReadLine(); // Needs work -> try/catch + loop or Int.TryParse + loop
+            string userInput = Console.ReadLine()!; 
             int number = Convert.ToInt32(userInput);
             return number;
         }
 
-        /// <summary>
-        /// Asks the user for a number within a certain range [min, max]. If the number is not in the range, re-prompt the user for a new number.
-        /// Will crash if the user input is not convertible to an int (throw exception?)
-        /// </summary>
-        /// <param name="text">Text to prompt the user</param>
-        /// <param name="min">Smallest permissible value</param>
-        /// <param name="max">Largest permissible value</param>
-        /// <returns>The user input as an integer</returns>
+        
         public static int AskForNumber(string text, int min, int max)
         {
             int userInput = AskForNumber(text);
-            // While the user input is outside the expected range
+
+            // if the number given by user is out of maximum range.
             while (userInput < min || userInput > max)
             {
                 userInput = AskForNumber("Your previous input is not valid, try again.");
@@ -51,28 +38,35 @@
             return userInput;
         }
     }
-
     static class Program
     {
         static void Main()
         {
             const int Min = 0;
-            const int Max = 10;
+            const int Max = 15;
             const int PrintOffset = 4;
 
-            int size = ArrayReplicator.AskForNumber("Enter the array size: ", Min, Max);
-            int[] original = new int[size];
 
-            // Fill the original array with user specified integers
+            int content = ArrayReplicator.AskForNumber("Enter the array content: ", Min, Max);
+            int[] real = new int[content];
+
+
+
+            // Fill up the real array with numbers mentioned by user.
+            
             for (int item = 0; item < size; ++item)
             {
-                original[item] = ArrayReplicator.AskForNumber("Enter a number: ");
+                real[item] = ArrayReplicator.AskForNumber("Enter a number: ");
             }
 
-            int[] copy = ArrayReplicator.ReplicateArray(original);
-            // Verify original and replicated array are the same
+
+            int[] print = ArrayReplicator.ReplicateArray(real);
+
+            // Verifing that real and duplicated arrays are the identical
+
             for (int index = 0; index < size; ++index)
-                Console.WriteLine($"Original {original[index],-PrintOffset}  {copy[index],4} Copy");
+                Console.WriteLine($"real {real[index],-PrintOffset}  {print[index],4} print");
         }
     }
 }
+
